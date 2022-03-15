b29c7af6d8762036d942e39e40740f5e16cba0fe
Lisa Meyer <lisamermey@gmail.com>
2022-03-15 14:15:09 -0400

    initial commit with psuedocode

---
+++ timer1.js
@@ -0,0 +1,15 @@
+/**
+ * Creates an alarm clock/timer that will beep after a specified amount of time has passed. 
+ * The user can specify an unlimited number of alarms using command line arguments.
+ */
+
+// get command line arguments and store in an array
+// iterate through the array of command line arguments to execute beep sound after specified time delay
+
+// EDGE CASES
+// do not beep if no numbers are provided
+// if input is a negative number, ignore or skip that argument
+// if the input is not a number, ignore or skip that argument
+
+//alarm sound:
+process.stdout.write('\x07');
