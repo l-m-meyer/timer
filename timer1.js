918d9e2ebc4578890ffac0f0166d8ac3abb2b885
Lisa Meyer <lisamermey@gmail.com>
2022-03-15 14:50:04 -0400

    working solution

--- timer1.js
+++ timer1.js
@@ -8,27 +8,26 @@ const soundAlarm = () => process.stdout.write('\x07');
 
 const timer = () => {
   // get command line arguments and store in an array
-  const beeps = process.argv.slice(2);
+  const args = process.argv.slice(2);
+  const beeps = args.map(time => Number(time));
   
   // checks if no numbers were provided
   if (!beeps.length) {
     return;
   }
 
-  // iterate through the array of command line arguments to execute beep sound after specified time delay
+  // iterate through the array of command line arguments
   for (const time of beeps) {
     // checks if beep time is a negative number
-    if (Number(time) < 0) {
-      continue;
-    }
+    if (time < 0) continue;
+
+    // checks if beep time is not a number
+    if (isNaN(time)) continue;
+    
+    // sounds the alarms after specified delay
     setTimeout(() => {
       soundAlarm();
-      console.log('I beeped...');
-    }, Number(time * 1000));
+    }, time * 1000);
   }
 };
 timer();
-
-// EDGE CASES
-// if input is a negative number, ignore or skip that argument
-// if the input is not a number, ignore or skip that argument
