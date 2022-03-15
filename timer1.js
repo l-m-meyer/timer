9afb62a9a3b30d93fa54ca1881741788d32afa61
Lisa Meyer <lisamermey@gmail.com>
2022-03-15 14:37:01 -0400

    added edge cases to check if no command line arguments entered and if the argument was a negative number

--- timer1.js
+++ timer1.js
@@ -6,18 +6,29 @@
 //alarm sound
 const soundAlarm = () => process.stdout.write('\x07');
 
-// get command line arguments and store in an array
-const beeps = process.argv.slice(2);
+const timer = () => {
+  // get command line arguments and store in an array
+  const beeps = process.argv.slice(2);
+  
+  // checks if no numbers were provided
+  if (!beeps.length) {
+    return;
+  }
 
-// iterate through the array of command line arguments to execute beep sound after specified time delay
-for (const time of beeps) {
-  setTimeout(() => {
-    soundAlarm();
-    console.log('I beeped...');
-  }, Number(time * 1000));
-}
+  // iterate through the array of command line arguments to execute beep sound after specified time delay
+  for (const time of beeps) {
+    // checks if beep time is a negative number
+    if (Number(time) < 0) {
+      continue;
+    }
+    setTimeout(() => {
+      soundAlarm();
+      console.log('I beeped...');
+    }, Number(time * 1000));
+  }
+};
+timer();
 
 // EDGE CASES
-// do not beep if no numbers are provided
 // if input is a negative number, ignore or skip that argument
 // if the input is not a number, ignore or skip that argument
