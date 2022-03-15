641256aa5b4d14bf49c07c98d8ffd2c329db55a3
Lisa Meyer <lisamermey@gmail.com>
2022-03-15 14:28:27 -0400

    successfully iterates through array of beep times and sounds the alarm

--- timer1.js
+++ timer1.js
@@ -3,15 +3,21 @@
  * The user can specify an unlimited number of alarms using command line arguments.
  */
 
+//alarm sound
+const soundAlarm = () => process.stdout.write('\x07');
+
 // get command line arguments and store in an array
 const beeps = process.argv.slice(2);
 
 // iterate through the array of command line arguments to execute beep sound after specified time delay
+for (const time of beeps) {
+  setTimeout(() => {
+    soundAlarm();
+    console.log('I beeped...');
+  }, Number(time * 1000));
+}
 
 // EDGE CASES
 // do not beep if no numbers are provided
 // if input is a negative number, ignore or skip that argument
 // if the input is not a number, ignore or skip that argument
-
-//alarm sound:
-process.stdout.write('\x07');
